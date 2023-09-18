fbb71702fb5178c2191a77dfa2a3f09288f244d4
Diego Córdova <A01781166@tec.mx>
2023-09-17 21:33:39 -0600

    hola

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+window.addEventListener("load", function() {
+    // Obtén el elemento que se desvanecerá
+    var fadeElement = document.getElementById("fade-in");
+
+    // Establece un temporizador para mostrar el elemento después de un breve retraso
+    setTimeout(function() {
+        // Muestra el elemento y cambia su opacidad a 1 (efecto de fade)
+        fadeElement.style.display = "block";
+        fadeElement.style.opacity = 1;
+    }, 1000); // 1000 ms = 1 segundo
+});
+
 var textoRecargar = document.getElementById("texto-recargar");
 
 textoRecargar.addEventListener("click", function(){

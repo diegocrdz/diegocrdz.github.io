10b42e9d41d34a60b8b0940283d02be20f0b90ec
Diego Córdova <A01781166@tec.mx>
2024-01-04 10:10:01 -0600

    index, style and script files are modified

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function ()
+{
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector("nav ul");
 
-    menuToggle.addEventListener("click", function () {
+    menuToggle.addEventListener("click", function ()
+    {
         navLinks.classList.toggle("active");
     });
+
+    window.addEventListener("scroll", function ()
+    {
+        const inicioSection = document.querySelector("#inicio");
+        const inicioSectionHeight = inicioSection.offsetHeight;
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition < inicioSectionHeight * 0.20)
+        {
+            document.querySelector("header").style.transition = "background-color 0.3s ease";
+            document.querySelector("header").style.backgroundColor = "transparent";
+        } else
+        {
+            document.querySelector("header").style.transition = "background-color 0.3s ease";
+            document.querySelector("header").style.backgroundColor = "#313131";
+        }
+    });
 });
+

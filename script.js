e1732088c67348b1e1affc71e3d8ae07139e1e7c
Diego Córdova <A01781166@tec.mx>
2024-02-03 12:06:37 -0600

    header modified

--- script.js
+++ script.js
@@ -7,21 +7,4 @@ document.addEventListener("DOMContentLoaded", function ()
     {
         navLinks.classList.toggle("active");
     });
-
-    window.addEventListener("scroll", function ()
-    {
-        const inicioSection = document.querySelector("#inicio");
-        const inicioSectionHeight = inicioSection.offsetHeight;
-        const scrollPosition = window.scrollY;
-
-        if (scrollPosition < inicioSectionHeight * 0.20)
-        {
-            document.querySelector("header").style.transition = "background-color 0.3s ease";
-            document.querySelector("header").style.backgroundColor = "transparent";
-        } else
-        {
-            document.querySelector("header").style.transition = "background-color 0.3s ease";
-            document.querySelector("header").style.backgroundColor = "#313131";
-        }
-    });
 });

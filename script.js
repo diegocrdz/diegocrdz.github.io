f8722d65c1e963a33c1d7e9afb0f6a1bd1b88585
diegocordova <A01781166@tec.mx>
2024-09-08 12:45:55 -0600

    header modified

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function ()
 {
     const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector("nav ul");
+    const navLinks = document.querySelector(".header nav ul");
 
     menuToggle.addEventListener("click", function ()
     {
         navLinks.classList.toggle("active");
+    });
+});
+
+// Add header to the HTML file
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
     });
 });

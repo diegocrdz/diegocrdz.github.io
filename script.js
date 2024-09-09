aeb3d334d3be933a1c04e63a36ecc7d6c6c9bbd0
Diego Córdova Rodríguez <A01781166@tec.mx>
2024-09-09 12:45:21 -0600

    modified header.css and script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-document.addEventListener("DOMContentLoaded", function ()
-{
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".header nav ul");
+// Hide the html extension from the URL
 
-    menuToggle.addEventListener("click", function ()
-    {
-        navLinks.classList.toggle("active");
-    });
-});
+if (window.location.pathname.endsWith('.html')) {
+    window.history.replaceState(null, null, window.location.pathname.replace('.html', ''));
+}
 
 // Add header to the HTML file
 
@@ -16,5 +11,18 @@ document.addEventListener("DOMContentLoaded", function() {
     .then(response => response.text())
     .then(data => {
         document.getElementById("header").innerHTML = data;
+    });
+});
+
+// Show and hide the navigation menu
+
+document.addEventListener("DOMContentLoaded", function ()
+{
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".header nav ul");
+
+    menuToggle.addEventListener("click", function ()
+    {
+        navLinks.classList.toggle("active");
     });
 });

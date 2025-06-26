55be2819696642f2310d4abd667c8d52f3ff44cd
diegocordova <A01781166@tec.mx>
2025-06-26 11:50:41 -0600

    Botón para cambiar de idioma agregado

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-// Add header to the HTML file
+// General script for the website
 
+// Add header to the HTML file
 document.addEventListener("DOMContentLoaded", function() {
     fetch('header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById("header").innerHTML = data;
 
-        // Después de que el header se ha cargado, añade los manejadores de eventos
+        // After the header is loaded, set up the menu toggle functionality
         const menuToggle = document.querySelector(".menu-toggle");
         const navLinks = document.querySelector("nav ul");
 
